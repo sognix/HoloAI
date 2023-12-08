b7c68015d23a53d6e7da14f99a60cef44660bfb1
unknown <sognix@sognix.at>
2023-12-08 05:58:57 +0100

    test of audio2text

--- PrepareData/Audio2Text.py
+++ PrepareData/Audio2Text.py
@@ -40,13 +40,17 @@ def audio_to_text(audio_path, model_path):
     # Erstelle einen Vosk-Recognizer mit dem geladenen Modell
     rec = vosk.KaldiRecognizer(vosk_model, audio_file.getframerate())
 
+    result = ""
+
     # Lese den Inhalt des Audiofiles
     while True:
         data = audio_file.readframes(4000)  # Lese 4000 Frames auf einmal
         if len(data) == 0:
             break
         if rec.AcceptWaveform(data):
-            result = rec.Result()
+            recResult = rec.Result()
+            debug(recResult)
+            result += recResult
 
     # Überprüfe, ob es noch nicht akzeptierte Daten gibt
     result += rec.FinalResult()
@@ -59,15 +63,22 @@ def audio_to_text(audio_path, model_path):
 if __name__ == "__main__":
     mp3_file = "TrainingData/Audio/MP3/s01e1.mp3"
     wav_file = "TrainingData/Audio/WAV/s01e1.wav"
-    model_path = "PrepareData/Model/vosk-model-de-0.21"
-    output_path = "TrainingData/Transcription/Audio2Text/s01e1.txt"
+
 
     debug("CONVERT MP3 TO WAV")
     convert_mp3_to_wav(mp3_file, wav_file)
 
-    debug("CONVERT WAV TO TXT")
+    debug("CONVERT WAV TO TXT MODEL1")
+    model_path = "PrepareData/Model/vosk-model-de-0.21"
+    output_path = "TrainingData/Transcription/Audio2Text/s01e1_Model1.txt"
     transcription = audio_to_text(wav_file, model_path)
     write_to_txt(transcription, output_path)
+
+    #debug("CONVERT WAV TO TXT MODEL2")
+    #model_path = "PrepareData/Model/vosk-model-de-tuda-0.6-900k"
+    #output_path = "TrainingData/Transcription/Audio2Text/s01e1_Model2.txt"
+    #transcription = audio_to_text(wav_file, model_path)
+    #write_to_txt(transcription, output_path)
 
     debug("DELETE WAV")
     delete_file(wav_file)
